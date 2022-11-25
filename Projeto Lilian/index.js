50881aef91f5889c18856ac177d618507eb13365
MaranaFabiano <86888732+MaranaFabiano@users.noreply.github.com>
2022-11-25 11:44:33 -0300

    Add files via upload

---
+++ Projeto Lilian/index.js
@@ -0,0 +1,129 @@
+var res1 = document.getElementsByName('botao1');
+var res2 = document.getElementsByName('botao2');
+var res3 = document.getElementsByName('botao3');
+var res4 = document.getElementsByName('botao4');
+var res5 = document.getElementsByName('botao5');
+var res6 = document.getElementsByName('botao6');
+var res7 = document.getElementsByName('botao7');
+var res8 = document.getElementsByName('botao8');
+var res9 = document.getElementsByName('botao9');
+var res10 = document.getElementsByName('botao10');
+var res11 = document.getElementsByName('botao11');
+var res12 = document.getElementsByName('botao12');
+var res13 = document.getElementsByName('botao13');
+var res14 = document.getElementsByName('botao14');
+var res15 = document.getElementsByName('botao15');
+var res16 = document.getElementsByName('botao16');
+var res17 = document.getElementsByName('botao17');
+var res18 = document.getElementsByName('botao18');
+var res19 = document.getElementsByName('botao19');
+var res20 = document.getElementsByName('botao20');
+var res21 = document.getElementsByName('botao21');
+let respostas = [];
+
+
+for (let key = 0; key < res1.length; key++) {
+    res1[key].addEventListener("change", (e) => {
+        armazenar(e.path);
+    });
+}
+for (let key = 0; key < res2.length; key++) {
+    res1[key].addEventListener("change", (e) => {
+        armazenar(e.path);
+    });
+}
+for (let key = 0; key < res3.length; key++) {
+    res1[key].addEventListener("change", (e) => {
+        armazenar(e.path);
+    });
+}
+for (let key = 0; key < res4.length; key++) {
+    res1[key].addEventListener("change", (e) => {
+        armazenar(e.path);
+    });
+}
+for (let key = 0; key < res5.length; key++) {
+    res1[key].addEventListener("change", (e) => {
+        armazenar(e.path);
+    });
+}
+for (let key = 0; key < res6.length; key++) {
+    res1[key].addEventListener("change", (e) => {
+        armazenar(e.path);
+    });
+}
+for (let key = 0; key < res7.length; key++) {
+    res1[key].addEventListener("change", (e) => {
+        armazenar(e.path);
+    });
+}
+for (let key = 0; key < res8.length; key++) {
+    res1[key].addEventListener("change", (e) => {
+        armazenar(e.path);
+    });
+}
+for (let key = 0; key < res9.length; key++) {
+    res1[key].addEventListener("change", (e) => {
+        armazenar(e.path);
+    });
+}
+for (let key = 0; key < res10.length; key++) {
+    res1[key].addEventListener("change", (e) => {
+        armazenar(e.path);
+    });
+}
+for (let key = 0; key < res11.length; key++) {
+    res1[key].addEventListener("change", (e) => {
+        armazenar(e.path);
+    });
+}
+for (let key = 0; key < res12.length; key++) {
+    res1[key].addEventListener("change", (e) => {
+        armazenar(e.path);
+    });
+}
+for (let key = 0; key < res13.length; key++) {
+    res1[key].addEventListener("change", (e) => {
+        armazenar(e.path);
+    });
+}
+for (let key = 0; key < res14.length; key++) {
+    res1[key].addEventListener("change", (e) => {
+        armazenar(e.path);
+    });
+}
+for (let key = 0; key < res15.length; key++) {
+    res1[key].addEventListener("change", (e) => {
+        armazenar(e.path);
+    });
+}
+for (let key = 0; key < res16.length; key++) {
+    res1[key].addEventListener("change", (e) => {
+        armazenar(e.path);
+    });
+}
+for (let key = 0; key < res17.length; key++) {
+    res1[key].addEventListener("change", (e) => {
+        armazenar(e.path);
+    });
+}
+for (let key = 0; key < res18.length; key++) {
+    res1[key].addEventListener("change", (e) => {
+        armazenar(e.path);
+    });
+}
+for (let key = 0; key < res19.length; key++) {
+    res1[key].addEventListener("change", (e) => {
+        armazenar(e.path);
+    });
+}
+for (let key = 0; key < res20.length; key++) {
+    res1[key].addEventListener("change", (e) => {
+        armazenar(e.path);
+    });
+}
+for (let key = 0; key < res21.length; key++) {
+    res1[key].addEventListener("change", (e) => {
+        armazenar(e.path);
+    });
+}
